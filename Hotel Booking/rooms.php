54dd51cab14ceea356460265819beb98fdc8856a
Chirag Malpani <56476770+chirs21@users.noreply.github.com>
2020-04-04 21:09:13 +0530

    Update rooms.php

--- Hotel Booking/rooms.php
+++ Hotel Booking/rooms.php
@@ -7,34 +7,27 @@
 <style>
 	body {
             background-image: url('img/2.jpg');
-            /*opacity: 0.5;*/
             background-repeat: no-repeat;
             background-attachment: fixed;
             width: 100%;
             height: auto;
             background-size: cover;
-            /*object-fit: contain;*/
         }
 	
 	.container-fluid{
-		/*border: 1px solid black;*/
 		margin-top: 80px;
-		/*width: 100%;*/
-
-	}
+}
 	.well{
 		margin-left: 0px;
 	}
 	.heading{
 		text-align: center;
-		/*border: 1px solid black;*/
 		height: 50px;	
 		padding-top: 2%;
 
 
 	}
 	.box{
-		/*border: 1px solid black;*/
 		width: 700px;
 		height: 305px;
 		padding-top: 5px;
@@ -98,7 +91,6 @@
                             </div>
                             </div>   
                             <div class='box'>
-                            <!-- <div class='col-md-4'></div> -->
                             <div class='col-md-5 well2'>
                                 <h2><a href="book.php" class="b">DOUBLE</a></h5><hr>
                                	<h6><a href="" class="a">No of Beds: 2</a></h6>
@@ -108,7 +100,6 @@
                             </div>
                             </div> 
                               <div class='box'>
-                            <!-- <div class='col-md-4'></div> -->
                             <div class='col-md-5 well3'>
                                 <h2><a href="book.php" class="b">DELUXE</a></h5><hr>
                                 <h6><a href="" class="a">No of Beds: 2</a></a></h6>
@@ -119,7 +110,6 @@
                             </div> 
 
   								<div class='box'>
-                            <!-- <div class='col-md-4'></div> -->
                             <div class='col-md-5 well4'>
                                 <h2><a href="book.php" class="b">SUPER DELUXE</a></h5><hr>
                                 <h6><a href="" class="a">No of Beds: 2</a></a></h6>
@@ -129,19 +119,6 @@
                             </div>
                             </div> 
 
-
-
              </div>
-
-
-
-
-			
-			<!-- <div class="type">DOUBLE</div>
-			<div class="type">DELUXE</div>
-			<div class="type">SUPER DELUXE</div> -->
-		
-		
-
 </body>
 </html>
